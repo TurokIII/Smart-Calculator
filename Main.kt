5018188445b6db5e4d3cb2fca84b271536cf6853
Arturo Hernandez <arturo.hernandez5@gmail.com>
2021-01-25 17:08:53 -0600

    added loop to continuously add int pairs, and can handle empty input or single int input

--- Main.kt
+++ Main.kt
@@ -3,7 +3,23 @@ import java.util.Scanner
 
 fun main() {
     val scan = Scanner(System.`in`)
-    val num1 = scan.nextInt()
-    val num2 = scan.nextInt()
-    println (num1 + num2)
+    while (true) {
+        val input = scan.nextLine()
+        if (input == "/exit") break
+        if (input == "") continue
+        val nums = input.split(" ").toTypedArray()
+
+        println(addNumbers(nums))
+    }
+    println("Bye!")
+}
+
+fun addNumbers(nums: Array<String>): Int {
+    var sum = 0
+
+    for (s in nums) {
+        if (s != " ") sum += s.toInt()
+    }
+
+    return sum
 }
