f5fd26959e8dd66ad04231f3e642ad1740ce7249
Arturo Hernandez <arturo.hernandez5@gmail.com>
2021-01-25 16:32:18 -0600

    stage 1: taking 2 numbers from standard input and printing sum

---
+++ Main.kt
@@ -0,0 +1,9 @@
+package calculator
+import java.util.Scanner
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    val num1 = scan.nextInt()
+    val num2 = scan.nextInt()
+    println (num1 + num2)
+}
