3eeb226ad7d6287f3e40cd44604a7b84a4ee596d
Arturo Hernandez <arturo.hernandez5@gmail.com>
2021-01-27 16:17:06 -0600

    added exception handling for invalid expressions and invalid commands. Added function for cleaning user input of excess symbols

--- Main.kt
+++ Main.kt
@@ -1,25 +1,128 @@
 package calculator
 import java.util.Scanner
+import kotlin.math.abs
 
 fun main() {
+    getInput()
+}
+
+fun getInput() {
     val scan = Scanner(System.`in`)
     while (true) {
         val input = scan.nextLine()
-        if (input == "/exit") break
-        if (input == "") continue
-        val nums = input.split(" ").toTypedArray()
+        if (input == "/exit") break else parseInputForAction(input)
 
-        println(addNumbers(nums))
+
     }
     println("Bye!")
 }
 
-fun addNumbers(nums: Array<String>): Int {
-    var sum = 0
+fun parseInputForAction(input: String) {
+    when {
+        input == "/help" -> println("The program calculates the sum of numbers")
+        input == "" -> {}
+        input[0] == '/' -> println("Unknown command")
+        else -> {
+            try {
+                val sum = calculateResult(input)
+                println(sum)
+            } catch (e: Exception) {
+                println("Invalid expression")
+            }
+        }
+    }
+}
 
-    for (s in nums) {
-        if (s != " ") sum += s.toInt()
+fun calculateResult(input: String): Int {
+    var total = 0
+    val cleanInput = cleanInput(input)
+    val numbers = parseNumbers(cleanInput)
+    val operators = parseOperators(cleanInput)
+
+    val delta = abs(numbers.size - operators.size)
+    if (delta != 1) throw Exception()
+
+    for (i in numbers.indices) {
+        if (i == 0) {
+            total += numbers[0].toInt()
+        } else {
+            val operator = operators[i - 1]
+            if (operator == "+") total += numbers[i].toInt() else total -= numbers[i].toInt()
+        }
     }
 
-    return sum
+    return total
 }
+
+fun cleanInput(input: String): String {
+    var result = input
+    while (result.contains("++")) { result = result.replace("++", "+") }
+    while (result.contains("--")) { result = result.replace("--", "+") }
+    while (result.contains("-+")) { result = result.replace("-+", "-") }
+    while (result.contains("+-")) { result = result.replace("+-", "-") }
+    while (result.contains("++")) { result = result.replace("++", "+") }
+
+    return result.replace(" ", "")
+}
+
+fun parseNumbers(input: String):Array<String> {
+    var numbers = ""
+    var numString = ""
+
+    for (i in input.indices) {
+        val char = input[i]
+        if (i == 0 ) {
+            if (char == '-') {
+                numString += char
+                continue
+            }
+            if (char == '+') {
+                continue
+            }
+        }
+        if (char.isDigit()) {
+            numString += char
+        } else {
+            numbers += " $numString"
+            numString = ""
+        }
+    }
+    if (numString.isNotEmpty()) numbers += " $numString"
+
+    return numbers.trim().split(" ").toTypedArray()
+}
+
+fun parseOperators(input: String):Array<String> {
+    var operators = ""
+
+    for (i in input.indices) {
+        val char = input[i]
+        if (i == 0 && char in "-+")  continue
+        if (char.isDigit()) continue
+        if (char in "-+") {
+            operators += " $char"
+        }
+    }
+
+    return if (operators == "") emptyArray() else operators.trim().split(" ").toTypedArray()
+}
+
+//    for (c in cleanInput) {
+//        if (c.isDigit()) {
+//            numString += c
+//        } else {
+//            if (c !in "-+") throw Exception()
+//            if (numString.isNotEmpty()) {
+//                if (addition == "add") total += numString.toInt() else total -= numString.toInt()
+//                numString = ""
+//                addition = ""
+//            }
+//            addition = if (c == '-') "sub" else "add"
+//        }
+//    }
+//
+//    if (numString.isNotEmpty()) {
+//        if (addition == "add") total += numString.toInt() else total -= numString.toInt()
+//    }
+//
+//    return total
